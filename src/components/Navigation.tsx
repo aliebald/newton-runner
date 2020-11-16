646ce40a08a8bbba7e40f50bb54c5d8707f0889d
Alexander Liebald <alexander.liebald@gmail.com>
2020-11-16 20:18:27 +0100

    [ITBL-10] Corrected navigation routing back to single page routing

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,15 +6,26 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 export default function Navigation(): ReactElement {
 	return (
 		<Navbar bg="dark" variant="dark" expand="lg">
-			<Navbar.Brand href="/">Physics Game</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="/">Landing Page</Nav.Link>
-					<Nav.Link href="/Tutorial">Tutorial</Nav.Link>
-					<NavDropdown title="Example level" id="basic-nav-dropdown">
-						<NavDropdown.Item href="/ExampleQuest1">Example Quest 1</NavDropdown.Item>
-						<NavDropdown.Item href="/ExampleQuest2">Example Quest 2</NavDropdown.Item>
+			<Link to="/" className="navbar-brand">
+				Physics Game
+			</Link>
+			<Navbar.Toggle aria-controls="navbar-nav" />
+			<Navbar.Collapse id="navbar-nav">
+				<Nav className="mr-auto navLink">
+					<Link to="/" className="navElem">
+						Landing Page
+					</Link>
+					<Link to="/Tutorial" className="navElem">
+						Tutorial
+					</Link>
+
+					<NavDropdown title="Example level" id="nav-dropdown">
+						<Link to="/ExampleQuest1" className="dropdown-item">
+							Example Quest 1
+						</Link>
+						<Link to="/ExampleQuest2" className="dropdown-item">
+							Example Quest 2
+						</Link>
 					</NavDropdown>
 				</Nav>
 			</Navbar.Collapse>

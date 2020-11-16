58f9586e6fb7ccae735e6d80d906c177ad4a2ad9
Alexander Liebald <alexander.liebald@gmail.com>
2020-11-16 20:28:07 +0100

    [ITBL-6] Fixed import error

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./../css/styles.css";
+import "./css/styles.css";
 
 ReactDOM.render(
 	<React.StrictMode>

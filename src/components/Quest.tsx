f9e607462c13520085066d0ee810186f3dd48a5d
Alexander Liebald <alexander.liebald@gmail.com>
2020-12-06 16:28:43 +0100

    [ITBL-24] Updated design for Quest component

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -15,9 +15,9 @@ export interface QuestConfig {
 export default function Quest(props: { config: QuestConfig }): ReactElement {
 	return (
 		<Container fluid>
-			<Row className="mx-2 mt-1">
+			<Row className="mx-auto mt-1 boxWrapper">
 				<Col sm="12" md="6">
-					<h2 className="text-left">{props.config.title}</h2>
+					<h2 className="text-left title">{props.config.title}</h2>
 					<p className="text-left">{props.config.description}</p>
 					<div className="pt-3">
 						<GraphInput cfg={props.config.graph}></GraphInput>
@@ -25,31 +25,47 @@ export default function Quest(props: { config: QuestConfig }): ReactElement {
 				</Col>
 				<Col sm="12" md="6">
 					<GameComponent settings={props.config.game} data={props.config.graph.data} />
-					<div className="pt-2">
-						<Button variant="primary" id="startGameBtn" className="px-3 mx-1">
-							Spiel&nbsp;starten
-						</Button>
-						<Button variant="primary" id="restartGameBtn" className="px-3 mx-1">
-							Nochmal&nbsp;versuchen
-						</Button>
-						<Button variant="primary" id="cameraLeftBtn" className="px-3 arrowKey my-1">
-							<i className="arrowLeft"></i>
-						</Button>
-						<div className="arrowKeysVertical">
-							<Button variant="primary" id="cameraUpBtn" className="px-3 arrowKey">
-								<i className="arrowUp"></i>
+					<div className="btnWrapper">
+						<div className="btnWrapperLeft">
+							<Button variant="primary" id="startGameBtn" className="px-3 mr-1">
+								Spiel&nbsp;starten
 							</Button>
-							<Button variant="primary" id="cameraDownBtn" className="px-3 arrowKey">
-								<i className="arrowDown"></i>
+							<Button variant="primary" id="restartGameBtn" className="px-3 mx-1">
+								Nochmal&nbsp;versuchen
 							</Button>
 						</div>
-						<Button
-							variant="primary"
-							id="cameraRightBtn"
-							className="px-3 arrowKey my-1"
-						>
-							<i className="arrowRight"></i>
-						</Button>
+						<div className="btnWrapperRight">
+							<Button
+								variant="primary"
+								id="cameraLeftBtn"
+								className="px-3 arrowKey my-1"
+							>
+								<i className="arrowLeft"></i>
+							</Button>
+							<div className="arrowKeysVertical">
+								<Button
+									variant="primary"
+									id="cameraUpBtn"
+									className="px-3 arrowKey"
+								>
+									<i className="arrowUp"></i>
+								</Button>
+								<Button
+									variant="primary"
+									id="cameraDownBtn"
+									className="px-3 arrowKey"
+								>
+									<i className="arrowDown"></i>
+								</Button>
+							</div>
+							<Button
+								variant="primary"
+								id="cameraRightBtn"
+								className="px-3 arrowKey my-1"
+							>
+								<i className="arrowRight"></i>
+							</Button>
+						</div>
 					</div>
 				</Col>
 			</Row>

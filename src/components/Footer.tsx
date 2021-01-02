aa7df0590407ef8448e9b2246bbc4687e1dbcaf8
Alexander Liebald <alexander.liebald@gmail.com>
2021-01-02 12:08:29 +0100

    Added padding to footer to avoid it overlapping other content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,27 @@
 import React, { ReactElement } from "react";
-import { Link } from "react-router-dom";
 import { resetUserdata } from "../userdata";
 
 export default function Footer(): ReactElement {
 	return (
-		<div className="footer">
-			<div className="footerLeft">
-				<p className="footerText">&copy; copyright 2020</p>
-			</div>
-			<div className="footerRight">
-				<a onClick={resetUserdata} className="footerLink">
-					Fortschritt zur&uuml;cksetzen
-				</a>
-				<a href="mailto:physics.game.team@gmail.com" className="footerLink">
-					Bug&nbsp;Melden
-				</a>
-				<a href="mailto:physics.game.team@gmail.com" className="footerLink">
-					Feedback
-				</a>
-				<a href="mailto:physics.game.team@gmail.com" className="footerLink">
-					Kontakt
-				</a>
+		<div className="pt-4">
+			<div className="footer">
+				<div className="footerLeft">
+					<p className="footerText">&copy; copyright 2020</p>
+				</div>
+				<div className="footerRight">
+					<a onClick={resetUserdata} className="footerLink">
+						Fortschritt zur&uuml;cksetzen
+					</a>
+					<a href="mailto:physics.game.team@gmail.com" className="footerLink">
+						Bug&nbsp;Melden
+					</a>
+					<a href="mailto:physics.game.team@gmail.com" className="footerLink">
+						Feedback
+					</a>
+					<a href="mailto:physics.game.team@gmail.com" className="footerLink">
+						Kontakt
+					</a>
+				</div>
 			</div>
 		</div>
 	);

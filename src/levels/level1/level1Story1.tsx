08b8bd69fa979c9c8dcc513d3540400d7e87109e
Alexander Liebald <alexander.liebald@gmail.com>
2021-01-27 19:49:07 +0100

    Added images to level 1 story 1

--- src/levels/level1/level1Story1.tsx
+++ src/levels/level1/level1Story1.tsx
@@ -4,7 +4,7 @@ import { TheoryConfig } from "../../components/Theory";
 const config: TheoryConfig = {
 	id: "level1Story1",
 	slides: [
-		<p className="centerBox" key={0}>
+		<p className="centerBox d-block" key={0}>
 			Heute ist ein aufregender Tag. Du freust dich schon l&auml;nger darauf, das Haus deines
 			Gro&szlig;vaters zu erkunden. Dieser ist leider vor einiger Zeit verstorben, aber nicht
 			ohne dir zu erz&auml;hlen, dass er viele au&szlig;ergew&ouml;hnliche Dinge bei sich
@@ -13,12 +13,24 @@ const config: TheoryConfig = {
 			Ecke, unter staubigen Decken entdeckst du eine riesige, schwere Holztruhe. Nur mit
 			M&uuml;he kannst du den Deckel dieser hochstemmen und was du dann siehst,
 			verschl&auml;gt dir den Atem. Ein Haufen alter Pergamentrollen, zerknitterte Briefe und
-			vergilbte B&uuml;cher h&auml;ufen sich in einem einzigen Durcheinander. Fasziniert hebst
-			du verschiedene St&uuml;cke an liest ein paar Zeilen und kommst aus dem Staunen gar
-			nicht mehr raus. Niemals h&auml;ttest du gedacht, dass dein eigener Gro&szlig;vater
-			solche Geheimnisse im Dachboden versteckt hat.
+			vergilbte B&uuml;cher h&auml;ufen sich in einem einzigen Durcheinander.
+			<img
+				src="levels/level1/Holztruhe.png"
+				height="110"
+				style={{ float: "right" }}
+				className="mt-2 mx-2"
+			/>
+			Fasziniert hebst du verschiedene St&uuml;cke an liest ein paar Zeilen und kommst aus dem
+			Staunen gar nicht mehr raus. Niemals h&auml;ttest du gedacht, dass dein eigener
+			Gro&szlig;vater solche Geheimnisse im Dachboden versteckt hat.
 		</p>,
-		<p className="centerBox" key={1}>
+		<p className="centerBox d-block" key={1}>
+			<img
+				src="levels/level1/Schatzkarte.png"
+				height="140"
+				style={{ float: "right" }}
+				className="mb-2 mx-2"
+			/>
 			Nachdem du einige Zeit mit dem Durchbl&auml;ttern der B&uuml;cher verbracht hast,
 			f&auml;llt dein Blick auf eine ganz besonders sch&ouml;ne Pergamentrolle. Vorsichtig
 			rollst du diese auseinander und siehst sie dir an. Du musst zweimal hinsehen, bevor du
@@ -28,17 +40,24 @@ const config: TheoryConfig = {
 			das am Ende alles nur ein Scherz ist. Wenn schon jemand anders diesen Schatz gefunden
 			hat, oder es ihn vielleicht niemals gegeben hat?
 		</p>,
-		<p className="centerBox" key={2}>
+		<p className="centerBox d-block" key={2}>
 			Doch daran m&ouml;chtest du im Moment nicht denken. Dein Gro&szlig;vater war ein toller
 			Mann, niemals h&auml;tte er diese Karte behalten, wenn es diesen Schatz nicht wirklich
 			geben w&uuml;rde. Eine Zeit lang siehst du dir alles genauer an. Man sieht verschiedene
 			Stationen, an welchen wohl Schl&uuml;ssel versteckt sein sollen. „Die sind dann wohl zum
 			&Ouml;ffnen einer Truhe“, denkst du dir. Jetzt bist du dir sicher. Du willst diesen
 			Schatz finden, schon allein in Gedenken an deinen Gro&szlig;vater. Nachdem dieser
-			Entschluss gefasst ist, packst du einen Rucksack mit dem N&ouml;tigsten, nimmst die
-			Schatzkarte in die Hand und atmest einmal tief durch. „Das wird ein Abenteuer und ich
-			bin sehr gespannt, was ich finden werde.“ Mit diesen Gedanken trittst du aus der
-			Haust&uuml;re und machst dich auf den Weg.
+			Entschluss gefasst ist,{" "}
+			<img
+				src="levels/level1/rucksack.png"
+				height="150"
+				style={{ float: "left" }}
+				className="mt-2 mx-2"
+			/>
+			packst du einen Rucksack mit dem N&ouml;tigsten, nimmst die Schatzkarte in die Hand und
+			atmest einmal tief durch. „Das wird ein Abenteuer und ich bin sehr gespannt, was ich
+			finden werde.“ Mit diesen Gedanken trittst du aus der Haust&uuml;re und machst dich auf
+			den Weg.
 		</p>,
 		<p className="centerBox" key={3}>
 			Für den ersten markierten Ort brauchst du nicht lang. Auf einer Wiese nicht weit von dem
